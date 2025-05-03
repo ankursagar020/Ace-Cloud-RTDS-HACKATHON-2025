6773ae18ac416ac42986aac8514b3f08f4f34336
ankursagar020 <146479225+ankursagar020@users.noreply.github.com>
2025-05-03 17:41:01 +0530

    Create Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/layout/Navbar";
+import HeroSection from "@/components/sections/HeroSection";
+import FeaturesSection from "@/components/sections/FeaturesSection";
+import HowItWorksSection from "@/components/sections/HowItWorksSection";
+import DemoSection from "@/components/sections/DemoSection";
+import CTASection from "@/components/sections/CTASection";
+import Footer from "@/components/layout/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <HeroSection />
+        <FeaturesSection />
+        <HowItWorksSection />
+        <DemoSection />
+        <CTASection />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
